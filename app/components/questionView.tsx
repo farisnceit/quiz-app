a42306799dbb3901933002e77ce8f93a6aabeeaa
Mohamed Fariz <faris872010@gmail.com>
2024-03-06 18:51:05 +0530

    working on timer interval after each question

--- app/components/questionView.tsx
+++ app/components/questionView.tsx
@@ -1,59 +1,74 @@
-"use client"
+"use client";
 
-import { useState,useEffect } from "react";
+import { useState, useEffect, useRef, MutableRefObject } from "react";
 import SingleQuestion from "./SingleQuestion";
 
 type propType = {
-    questions : Object
+  questions: Object;
+};
+
+enum questionEnum {
+  timer = 5
 }
 
-export default function QuestionView({questions}: Readonly<propType>) {
+export default function QuestionView({ questions }: Readonly<propType>) {
+  console.log(questions);
+  
+  const [quest, setQuest] = useState(questions?.results);
+  const [current, setCurrent] = useState(0);
 
-    console.log(questions)
+  const [single, setSingle] = useState<Object>({});
+  let [time, setTime] = useState(questionEnum.timer);
 
-    const TOTAL_QUESTION = 10;
+  const totalQuestion: number = quest?.length ?? 0;
 
-    const [quest,setQuest] = useState(questions?.results)
-    const [current,setCurrent] = useState(0);    
-   
-    const [single, setSingle] = useState<Object>({});
-    let [time, setTime] = useState(30); 
+  let intervalRef = useRef<MutableRefObject<null>>(null);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-          if (time > 0) {
-            setTime(time - 1);
-          } else {
-            clearInterval(interval);
-            handleNextClick();
-            //router.push('/destination-page'); // Redirect to the destination page after countdown
-          }
-        }, 1000); // Run the interval every second
-    
-        return () => clearInterval(interval); // Cleanup the interval on component unmount
-      }, [time]);
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      if (time > 0) {
+        setTime(time - 1);
+      } else {
+        clearInterval(intervalRef.current);
+        handleNextClick();
+      }
+    }, 1000); // Run the interval every second
 
-   
+    return () => clearInterval(intervalRef.current); // Cleanup the interval on component unmount
+  }, [time]);
 
-    useEffect(() => {
-        setSingle(quest[current])
+  useEffect(() => {
+    setSingle(quest[current]);
 
-        console.log("single",single)
-    }, [current])
+    console.log("single", single);
+  }, [current]);
 
-
-    const handleNextClick = () =>{
-        console.log("Single")
-        setTime(30);
-        setCurrent(current+1);
-        
-    } 
-
-    
+  const handleNextClick = () => {
+    console.log("Single",totalQuestion-1,current);
     
 
-    return <main>
-        <SingleQuestion question={quest[current]} time={time} />
-        <button onClick={()=>{handleNextClick()}}>NEXT</button>
-        </main>
-  }
+    if(totalQuestion-1 != current){
+      setCurrent(current + 1);
+      setTime(questionEnum.timer);
+    }else{
+      console.log("Completed");
+      clearInterval(intervalRef.current)
+      intervalRef.current = null;
+    }
+    
+  };
+
+  return (
+    <main>
+      {current}
+      {(totalQuestion-1 != current) && <SingleQuestion question={quest[current]} time={time} />}
+      <button
+        onClick={() => {
+          handleNextClick();
+        }}
+      >
+        NEXT
+      </button>
+    </main>
+  );
+}
